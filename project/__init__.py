6518628ba51bc1fe2f847f13a17955991c71b792
tbeka <tbektemissov@gmail.com>
2024-02-25 17:05:59 +0100

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-login_manager = LoginManager()
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
+    login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
