da6313a7c0a58c9f919352fb18ee7b71b3c19243
tbeka <tbektemissov@gmail.com>
2024-02-26 17:13:37 +0100

    Renamed the folder

--- project/models.py
+++ project/models.py
@@ -2,7 +2,7 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model ):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True, nullable=False)  # Add nullable=False
     password = db.Column(db.String(100))
