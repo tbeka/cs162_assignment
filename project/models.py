18db815bd3bffa1cbc214cb371c42e95a9b605e1
tbeka <tbektemissov@gmail.com>
2024-02-26 16:26:21 +0100

    Update models.py

--- project/models.py
+++ project/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)  # Add nullable=False
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     #lists = db.relationship('List', backref='user', lazy=True)
