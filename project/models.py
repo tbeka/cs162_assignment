2859d2e9c1d7e0fb492c2e6f005f952464f5f42c
tbeka <tbektemissov@gmail.com>
2024-02-27 11:28:52 +0100

    Added lists to the database

--- project/models.py
+++ project/models.py
@@ -4,10 +4,10 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model ):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True, nullable=False)  # Add nullable=False
+    email = db.Column(db.String(100), unique=True)  # Add nullable=False
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-    #lists = db.relationship('List', backref='user', lazy=True)
+    lists = db.relationship('List', backref='user', lazy=True)
 
 class List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
